b7d0b0fb28507e6bbbc5e55428f0bb1cf037e74f
Kimyerim1935 <kimyerim1935@naver.com>
2021-12-22 15:05:29 +0900

    profile img change, img delete

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,16 @@
 import React,{ useState, useEffect } from 'react';
 import Img from '../assets/img/img.jpg';
 import Camera from '../components/svg/Camera';
+import Delete from '../components/svg/Delete';
 import { storage, db, auth } from '../firebase';
-import { ref, getDownloadURL, uploadBytes} from 'firebase/storage';
+import { ref, getDownloadURL, uploadBytes, deleteObject } from 'firebase/storage';
 import { getDoc, doc, updateDoc } from 'firebase/firestore';
-import { isCompositeComponent } from 'react-dom/test-utils';
+import { useHistory } from 'react-router-dom';
 
 const Profile = () => {
     const [img, setImg] = useState('')
     const [user, setUser] = useState();
+    const history = useHistory();
 
     useEffect(()=> {
         getDoc(doc(db, 'users', auth.currentUser.uid)).then(docSnap => {
@@ -20,7 +22,9 @@ const Profile = () => {
             const uploadImg = async () => {
                 const imgRef = ref(storage,
                     `avatar/${new Date().getTime()} - ${img.name}`);
-
+                    if (user.avatarPath) {
+                        await deleteObject(ref(storage, user.avatarPath))
+                    }
                 try {
                     const snap = await uploadBytes(imgRef, img);
                     const url = await getDownloadURL(ref(storage, snap.ref.fullPath ))
@@ -36,27 +40,45 @@ const Profile = () => {
             };
             uploadImg();
         }  
-    },[img])
+    },[img]);
+
+    const deleteImage = async () => {
+        try {
+            const confirm = window.confirm('Delete avatar?')
+            if (confirm) {
+                await deleteObject(ref(storage, user?.avatarPath))
+
+                await updateDoc(doc(db, 'users', auth.currentUser.uid), {
+                    avatar: '',
+                    avatarPath: ''
+                })
+                history.push('/')
+            }
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
 
     return (
         <section>
             <div className="profile_container">
                 <div className="img_container">
-                    <img src={Img} alt="img"/>
+                    <img src={user?.avatar || Img} alt="img"/>
                     <div className="overlay">
                         <div>
                             <label htmlFor="photo">
                                 <Camera />
                             </label>
+                            {user?.avatar ? <Delete deleteImage={deleteImage}/> : null}
                             <input type="file" accept='image/*' style={{ display:'none' }} id='photo' onChange={e=>setImg(e.target.files[0])} />
                         </div>
                     </div>
                 </div>
                 <div className="text_container">
-                    <h3>User name</h3>
-                    <p>User email</p>
+                    <h3>{user ? user.name : null}</h3>
+                    <p>{user ? user.email : null}</p>
                     <hr/>
-                    <small>Joined on: ...</small>
+                    <small>Joined on: {user?.createdAt.toDate().toDateString()}</small>
                 </div>
             </div>
         </section>
