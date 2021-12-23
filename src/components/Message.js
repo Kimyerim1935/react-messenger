8cbbed8900ba16aad37f370f12226840c37454a0
Kimyerim1935 <kimyerim1935@naver.com>
2021-12-23 19:01:32 +0900

    message useRef 적용

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useRef, useEffect} from 'react';
 import Moment from 'react-moment';
 
 const Message = ({ msg, user1 }) => {
+    const scrollRef = useRef();
+
+    useEffect(() => {
+        scrollRef.current?.scrollIntoView({ behavior: 'smooth'})
+    },[msg])
+
     return (
-        <div className={`message_wrapper ${msg.from === user1 ? 'own' : ''}`}>
+        <div className={`message_wrapper ${msg.from === user1 ? 'own' : ''}`} ref={scrollRef}>
             <p className={msg.from === user1 ? 'me' : 'friend'}>
                 {msg.media ? <img src={msg.media} alt={msg.text} /> : null}
                 {msg.text}
